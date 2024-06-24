e6a514bd262dabdb8f273de712f6f958a3d1819e
David Papp <pigri@users.noreply.github.com>
2024-06-24 14:25:30 +0200

    fix: Main test fix

--- main_test.go
+++ main_test.go
@@ -5,10 +5,10 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"testing"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/openshieldai/openshield/lib"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,74 +28,71 @@ func setupApp() *fiber.App {
 	return app
 }
 
-//func TestClean(t *testing.T) {
-//	settings := lib.NewSettings()
-//	hashedAPIKey, _ := hex.DecodeString(settings.OpenAI.APIKeyHash)
-//	store := redis.New(redis.Config{
-//		Host:  "127.0.0.1",
-//		Port:  6379,
-//		Reset: true,
-//	})
-//	err := store.Delete(fmt.Sprintf("%x", sha256.Sum256(hashedAPIKey)))
-//	if err != nil {
-//		return
-//	}
-//}
-
 func TestAuth(t *testing.T) {
 	app := setupApp() // Assuming you have a setupApp function that sets up your fiber app
 
 	req, _ := http.NewRequest("GET", "/openai/v1/models/gpt-4", nil)
-	resp, _ := app.Test(req)
+	resp, err := app.Test(req, 5000)
+	if err != nil {
+		t.Fatalf("app.Test returned an error: %v", err)
+	}
 
 	assert.Equal(t, 401, resp.StatusCode, "Expected status code 401")
-	//TestClean(t)
 }
 
 func TestListModels(t *testing.T) {
 	app := setupApp()
-	settings := lib.NewSettings()
-	//TestClean(t)
 
-	req, _ := http.NewRequest("GET", "/openai/v1/models", nil)
+	req, err := http.NewRequest("GET", "/openai/v1/models", nil)
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set(
 		"Authorization",
-		fmt.Sprintf("Bearer %s", settings.OpenAI.APIKey),
+		fmt.Sprintf("Bearer %s", os.Getenv("OPENSHIELD_API_KEY")),
 	)
-	resp, _ := app.Test(req)
+	if err != nil {
+		t.Fatalf("http.NewRequest returned an error: %v", err)
+	}
+
+	resp, err := app.Test(req, 5000)
+	if err != nil {
+		t.Fatalf("app.Test returned an error: %v", err)
+	}
 
 	assert.Equal(t, 200, resp.StatusCode, "Expected status code 200")
 }
 
 func TestGetModel(t *testing.T) {
 	app := setupApp()
-	settings := lib.NewSettings()
+	//settings := lib.NewSettings()
 	//TestClean(t)
 
 	req, _ := http.NewRequest("GET", "/openai/v1/models/gpt-4", nil)
 	req.Header.Set(
 		"Authorization",
-		fmt.Sprintf("Bearer %s", settings.OpenAI.APIKey),
+		fmt.Sprintf("Bearer %s", os.Getenv("OPENSHIELD_API_KEY")),
 	)
-	resp, _ := app.Test(req)
+	resp, err := app.Test(req, 5000)
+	if err != nil {
+		t.Fatalf("app.Test returned an error: %v", err)
+	}
 
 	assert.Equal(t, 200, resp.StatusCode, "Expected status code 200")
 }
 
 func TestTokenizer(t *testing.T) {
 	app := setupApp()
-	settings := lib.NewSettings()
-	//TestClean(t)
 
 	reqBody := bytes.NewBuffer([]byte("thisateststringfortokenizer"))
 	req, _ := http.NewRequest("POST", "/tokenizer/gpt-3.5", reqBody)
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set(
 		"Authorization",
-		fmt.Sprintf("Bearer %s", settings.OpenShield.APIKey),
+		fmt.Sprintf("Bearer %s", os.Getenv("OPENSHIELD_API_KEY")),
 	)
-	resp, _ := app.Test(req)
+	resp, err := app.Test(req, 5000)
+	if err != nil {
+		t.Fatalf("app.Test returned an error: %v", err)
+	}
 	bodyBytes, _ := io.ReadAll(resp.Body)
 	bodyString := string(bodyBytes)
 
@@ -109,10 +106,6 @@ func TestChatCompletion(t *testing.T) {
 		t.Fatal("setupApp returned nil")
 	}
 
-	settings := lib.NewSettings()
-
-	//TestClean(t)
-
 	reqBody := bytes.NewBuffer([]byte(`{"model":"gpt-4","messages":[{"role":"system","content":"You are a helpful assistant."},{"role":"user","content":"What is the meaning of life?"}]}`))
 	req, err := http.NewRequest("POST", "/openai/v1/chat/completions", reqBody)
 	if err != nil {
@@ -121,11 +114,11 @@ func TestChatCompletion(t *testing.T) {
 
 	req.Header.Set(
 		"Authorization",
-		fmt.Sprintf("Bearer %s", settings.OpenAI.APIKey),
+		fmt.Sprintf("Bearer %s", os.Getenv("OPENSHIELD_API_KEY")),
 	)
 	req.Header.Set("Content-Type", "application/json")
 
-	resp, err := app.Test(req, 10000)
+	resp, err := app.Test(req, 100000)
 	if err != nil {
 		t.Fatalf("app.Test returned an error: %v", err)
 	}
