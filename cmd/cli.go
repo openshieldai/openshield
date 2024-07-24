9d2a5ca71d7e578c189117051d85dad2c8080cc9
krichard1212 <richardkohegyi@gmail.com>
2024-07-24 16:02:39 +0200

    Implement start/stop

--- cmd/cli.go
+++ cmd/cli.go
@@ -5,6 +5,9 @@ import (
 	"github.com/openshieldai/openshield/server"
 	"github.com/spf13/cobra"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 var rootCmd = &cobra.Command{
@@ -103,11 +106,40 @@ var stopServerCmd = &cobra.Command{
 	Use:   "stop",
 	Short: "Stop the server",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Stopping the server...")
-		if err := server.StopServer(); err != nil {
+		fmt.Println("Sending stop signal to the server...")
+		if err := stopServer(); err != nil {
 			fmt.Printf("Error: %v\n", err)
 			os.Exit(1)
 		}
-		fmt.Println("Server stopped successfully")
+		fmt.Println("Stop signal sent to the server")
 	},
+}
+
+func stopServer() error {
+	// Create a channel to receive OS signals
+	sigs := make(chan os.Signal, 1)
+
+	// Register for SIGINT
+	signal.Notify(sigs, syscall.SIGINT)
+
+	// Send SIGINT to the current process
+	p, err := os.FindProcess(os.Getpid())
+	if err != nil {
+		return fmt.Errorf("failed to find current process: %v", err)
+	}
+
+	err = p.Signal(syscall.SIGINT)
+	if err != nil {
+		return fmt.Errorf("failed to send interrupt signal: %v", err)
+	}
+
+	// Wait for a short time to allow the signal to be processed
+	select {
+	case <-sigs:
+		fmt.Println("Interrupt signal received")
+	case <-time.After(2 * time.Second):
+		fmt.Println("No confirmation received, but signal was sent")
+	}
+
+	return nil
 }
