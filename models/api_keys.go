89effc44816dd9c77131b59292019f25337ed435
krichard1212 <richardkohegyi@gmail.com>
2024-07-26 09:50:19 +0200

    fix faker

--- models/api_keys.go
+++ models/api_keys.go
@@ -7,8 +7,8 @@ import (
 type ApiKeys struct {
 	Base      `gorm:"embedded"`
 	ProductID uuid.UUID `gorm:"product_id;not null"`
-	ApiKey    string    `gorm:"api_key;not null;uniqueIndex;index:idx_api_keys_status,unique"`
-	Status    Status    `sql:"status;not null;index:idx_api_keys_status,unique;type:enum('active', 'inactive', 'archived')"`
-	Tags      string    `gorm:"tags;<-:false"`
-	CreatedBy string    `gorm:"created_by;not null"`
+	ApiKey    string    `faker:"uuid_hyphenated" gorm:"api_key;not null;uniqueIndex;index:idx_api_keys_status,unique"`
+	Status    Status    `faker:"status" sql:"status;not null;index:idx_api_keys_status,unique;type:enum('active', 'inactive', 'archived')"`
+	Tags      string    `faker:"tags" gorm:"tags;<-:false"`
+	CreatedBy string    `faker:"name" gorm:"created_by;not null"`
 }
